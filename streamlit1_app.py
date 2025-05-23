6cf2b5e165a7d4642ab08a20f9d57cba958de4dd
xsolver88 <nathanielhake@gmail.com>
2025-05-22 23:32:01 -0400

    Update streamlit1_app.py

--- streamlit1_app.py
+++ streamlit1_app.py
@@ -47,4 +47,113 @@ const data = {data_json}.map(d => {{
   }};
 }});
 
-const margin
+const margin = {{top: 20, right: 30, bottom: 40, left: 60}},
+      width = 600 - margin.left - margin.right,
+      height = 300 - margin.top - margin.bottom;
+
+const svg = d3.select("#chart")
+  .html("")
+  .append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform", `translate(${{margin.left}},${{margin.top}})`);
+
+const x = d3.scaleTime()
+  .domain(d3.extent(data, d => d.date))
+  .range([0, width]);
+
+const y = d3.scaleLinear()
+  .domain([0, d3.max(data, d => d.sales)]).nice()
+  .range([height, 0]);
+
+svg.append("g")
+  .attr("transform", `translate(0,${{height}})`)
+  .call(d3.axisBottom(x).tickFormat(d3.timeFormat("%b")));
+
+svg.append("g").call(d3.axisLeft(y));
+
+// Axis labels
+svg.append("text")
+  .attr("x", width / 2)
+  .attr("y", height + margin.bottom - 5)
+  .style("text-anchor", "middle")
+  .text("Month");
+
+svg.append("text")
+  .attr("transform", "rotate(-90)")
+  .attr("x", -height / 2)
+  .attr("y", -margin.left + 15)
+  .style("text-anchor", "middle")
+  .text("Car Sales");
+
+// Area
+const area = d3.area()
+  .x(d => x(d.date))
+  .y0(height)
+  .y1(d => y(d.sales))
+  .curve(d3.curveMonotoneX);
+
+svg.append("path")
+  .datum(data)
+  .attr("fill", "steelblue")
+  .attr("d", area);
+
+// Tooltip line and circle
+const focus = svg.append("g").style("display", "none");
+
+focus.append("line")
+  .attr("class", "x-hover-line")
+  .attr("stroke", "gray")
+  .attr("stroke-width", 1)
+  .attr("y1", 0)
+  .attr("y2", height);
+
+focus.append("circle")
+  .attr("r", 5)
+  .attr("fill", "orange")
+  .attr("stroke", "black");
+
+const tooltip = d3.select("#chart").append("div")
+  .attr("class", "tooltip")
+  .style("opacity", 0);
+
+svg.append("rect")
+  .attr("width", width)
+  .attr("height", height)
+  .style("fill", "none")
+  .style("pointer-events", "all")
+  .on("mouseover", () => {{
+    focus.style("display", null);
+    tooltip.style("opacity", 1);
+  }})
+  .on("mouseout", () => {{
+    focus.style("display", "none");
+    tooltip.style("opacity", 0);
+  }})
+  .on("mousemove", (event) => {{
+    const bisectDate = d3.bisector(d => d.date).left;
+    const mouse = d3.pointer(event)[0];
+    const x0 = x.invert(mouse);
+    const i = bisectDate(data, x0, 1);
+    const d0 = data[i - 1];
+    const d1 = data[i];
+    const d = x0 - d0.date > d1.date - x0 ? d1 : d0;
+
+    focus.select("circle")
+      .attr("cx", x(d.date))
+      .attr("cy", y(d.sales));
+
+    focus.select(".x-hover-line")
+      .attr("x1", x(d.date))
+      .attr("x2", x(d.date));
+
+    tooltip.html(`<b>${{d3.timeFormat("%B")(d.date)}}</b><br/>Sales: ${{d.sales.toLocaleString()}}`)
+      .style("position", "fixed")
+      .style("left", (event.clientX + 10) + "px")
+      .style("top", (event.clientY - 40) + "px");
+  }});
+</script>
+"""
+
+components.html(html_code, height=420)
